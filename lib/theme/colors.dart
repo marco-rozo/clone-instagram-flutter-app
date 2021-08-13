46231f1f952fe8f928c443715b10e9503bcfd576
Marco Antonio <marcorozo99@gmail.com>
2021-08-13 00:51:11 -0300

    updated colors palet

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -5,6 +5,7 @@ const appFooterColor = Color(0xFF131313);
 const primary = Color(0xFF000000);
 const white = Color(0xFFFFFFFF);
 const black = Color(0xFF000000);
+const grey = Color(0xFFb7b7b7);
 const textFieldBackground = Color(0xFF262626);
 const buttonFollowColor = Color(0xFF0494F5);
 const storyBorderColor = [
